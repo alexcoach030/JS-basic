646ae69c91583bf4fb12ce86fec3e345cf2538f7
alexcoach <mvdrf030@gmail.com>
2021-03-11 05:30:02 +0200

    l7 done

--- basket.js
+++ basket.js
@@ -1,5 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    function filter(event){
+        let action = event.target.dataset.filter;
+        if (!action) return;
+        let catalog = document.getElementById('catalog');
+        if (action === 'all'){//если "Показать все товары"
+            catalog.innerHTML = '';
+            getCatalog();
+            return;
+        }
+        let products = catalog.querySelectorAll('.prod');//получаем все prod в каталоге
+        products.forEach(element => {//перебираем проды и удаляем лишние
+            if (element.dataset.type !== action) element.remove();
+        })
+    }
+
+    function getCatalog(){
+        for (let [key, value] of Object.entries(products)) {//Перебираю объект массивов с каталогом для вывода в DOM
+            let product = document.createElement('div');
+            product.className = 'prod';
+            product.dataset.id = key;
+            product.dataset.type = value.type;
+            let name = value.name;
+            let price = value.price;
+            product.innerHTML = `<p>Название:</p> <p>${name}</p><p>Цена: ${price}</p><button data-name="add" data-id="${key}">Добавить в корзину</button>`
+            catalog.append(product);
+        }
+    }
+
     function price() {
         sum = 0;
         for (let i = 0; i < basket.length; i++) {
@@ -32,19 +60,23 @@ document.addEventListener('DOMContentLoaded', () => {
     let products = {
         1: {
             name: 'bike',
-            price: 300
+            price: 300,
+            type: 'movable',
         },
         2: {
             name: 'car',
-            price: 500
+            price: 500,
+            type: 'movable',
         },
         3: {
             name: 'ship',
-            price: 1000
+            price: 1000,
+            type: 'movable',
         },
         4: {
             name: 'something',
-            price: 100
+            price: 100,
+            type: 'immovables',
         }
     }
 
@@ -57,15 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
     container.append(cart);
     cart.append(cartText);
 
-    for (let [key, value] of Object.entries(products)) {//Перебираю объект массивов с каталогом для вывода в DOM
-        let product = document.createElement('div');
-        product.className = 'prod';
-        product.dataset.id = key;
-        let name = value.name;
-        let price = value.price;
-        product.innerHTML = `<p>Название:</p> <p>${name}</p><p>Цена: ${price}</p><button data-name="add" data-id="${key}">Добавить в корзину</button>`
-        catalog.append(product);
-    }
+    getCatalog();
     document.addEventListener('click', addToCart);
-
+    document.addEventListener('click', filter);
 })
